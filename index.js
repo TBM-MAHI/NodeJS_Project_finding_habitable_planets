37dcd450d519e311f3690a4182e2eb76e04fa522
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2023-03-19 19:36:41 +0600

    completed final array and priniting planet names

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const { parse } = require("csv-parse");
 const fs = require('fs');
-const result = [];
+const habitablePlanets = [];
 /* 
 fs.createReadStream( ) retuns an instance of the class Class: fs.ReadStream
+fs.ReadStream has events that are being handeled/
  */
-fs.createReadStream('./keplar_data.csv').
-    on('data', (datas) => result.push(datas)).
-    on('error',(err)=>console.log(err)).
+fs.createReadStream('./keplar_data.csv', { encoding: 'utf-8' }).
+    pipe(parse({
+        comment: '#',
+        columns: true
+    }
+    )).
+    on('data', (planetData) => {
+        if (isHabitable(planetData)) 
+            habitablePlanets.push(planetData);
+    }).
+    on('error', (err) => console.log(err)).
     on('end', () => {
-        console.log(result);
+        console.log(habitablePlanets.length,"habitable planets found! \n They are :");
+        let habitablePlanetsNames = habitablePlanets.map((p) => p.kepler_name);
+        for (let names of habitablePlanetsNames)
+            console.log(`\t ${names}`);
         console.log('end');
-    })
+})
+
+function isHabitable(planet) {
+    //returns true or false
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
